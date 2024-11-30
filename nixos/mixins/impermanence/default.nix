dc12f1e435d9af6abc2a5e77fc96ade84e37f0db
Jacob Root <me@rooty.dev>
2024-11-30 15:47:00 -0800

    Fixed ssh fingerprints changing on every boot

--- nixos/mixins/impermanence/default.nix
+++ nixos/mixins/impermanence/default.nix
@@ -77,6 +77,10 @@
     files = [
       "/etc/machine-id"
       "/root/.bash_history"
+      "/etc/ssh/ssh_host_ed25519_key"
+      "/etc/ssh/ssh_host_ed25519_key.pub"
+      "/etc/ssh/ssh_host_rsa_key"
+      "/etc/ssh/ssh_host_rsa_key.pub"
     ];
     users.jrt = {
       directories = [
