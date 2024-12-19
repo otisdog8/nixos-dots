4b2619a5c5c68b62c7280efe42bc2e85db014d24
Jacob Root <me@rooty.dev>
2024-12-18 21:37:01 -0800

    Add machines

---
+++ nixos/arquitens/disks.nix
@@ -0,0 +1,74 @@
+_: {
+
+  boot.initrd.luks.devices."luks".device = "/dev/nvme0n1p2";
+
+  fileSystems."/" =
+    { device = "/dev/mapper/luks";
+      fsType = "btrfs";
+      options = [ "subvol=root" "compress=zstd" "noatime" ];
+    };
+
+  fileSystems."/mnt/btrfs_root" =
+    { device = "/dev/mapper/luks";
+      fsType = "btrfs";
+      neededForBoot = true;
+    };
+
+
+  fileSystems."/large" =
+    { device = "/dev/mapper/luks";
+      fsType = "btrfs";
+      options = [ "subvol=large" "compress=zstd" "noatime" ];
+      neededForBoot = true;
+    };
+
+
+  fileSystems."/cache" =
+    { device = "/dev/mapper/luks";
+      fsType = "btrfs";
+      options = [ "subvol=cache" "compress=zstd" "noatime" ];
+      neededForBoot = true;
+    };
+
+
+  fileSystems."/dots" =
+    { device = "/dev/mapper/luks";
+      fsType = "btrfs";
+      options = [ "subvol=cache" "compress=zstd" "noatime" ];
+      neededForBoot = true;
+    };
+
+
+  fileSystems."/persist" =
+    { device = "/dev/mapper/luks";
+      fsType = "btrfs";
+      options = [ "subvol=persist" "compress=zstd" "noatime" ];
+      neededForBoot = true;
+    };
+
+  fileSystems."/nix" =
+    { device = "/dev/mapper/luks";
+      fsType = "btrfs";
+      options = [ "subvol=nix" "compress=zstd" "noatime" ];
+      neededForBoot = true;
+    };
+
+  fileSystems."/mc" =
+    { device = "/dev/mapper/luks";
+      fsType = "btrfs";
+      options = [ "subvol=mc" "compress=zstd" "noatime" ];
+      neededForBoot = true;
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/nvme0n1p1";
+      fsType = "vfat";
+      options = [ "fmask=0022" "dmask=0022" ];
+    };
+
+  swapDevices = [ {
+    device = "/dev/nvme0n1p3";
+    randomEncryption.enable = true;
+  } ];
+
+}
