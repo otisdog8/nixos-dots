c2dadd0c43894122b7bfbb64650a5f2bb37d8c44
Jacob Root <me@rooty.dev>
2024-12-22 01:15:58 -0800

    Add ports to fw for k3s

--- nixos/mixins/k0s/default.nix
+++ nixos/mixins/k0s/default.nix
@@ -37,13 +37,12 @@
     9963 # cilium-operator Prometheus metrics
     9964 # cilium-envoy Prometheus metrics
     6443 # k3s: required so that pods can reach the API server (running on port 6443 by default)
-    # 2379 # k3s, etcd clients: required if using a "High Availability Embedded etcd" configuration
-    # 2380 # k3s, etcd peers: required if using a "High Availability Embedded etcd" configuration
+    2379 # k3s, etcd clients: required if using a "High Availability Embedded etcd" configuration
+    2380 # k3s, etcd peers: required if using a "High Availability Embedded etcd" configuration
   ];
 
   networking.firewall.allowedUDPPorts = [
     51871 # WireGuard encryption tunnel endpoint
-    # 8472 # k3s, flannel: required if using multi-node for inter-node networking
   ];
 
 
