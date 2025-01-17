a265467742b09b95bd8907eb1f31e09091cd7a36
Jacob Root <me@rooty.dev>
2025-01-17 15:28:42 -0800

    Add hidraw

--- nixos/default.nix
+++ nixos/default.nix
@@ -16,40 +16,47 @@
 }:
 
 {
-  imports =
-    [ # Include the results of the hardwae scan.
-      inputs.hyprland.nixosModules.default
-      inputs.impermanence.nixosModules.impermanence
-      inputs.lanzaboote.nixosModules.lanzaboote
-      inputs.chaotic.nixosModules.default # OUR DEFAULT MODULE
-      inputs.home-manager.nixosModules.home-manager
-      ./${hostname}
-      {
-        home-manager.extraSpecialArgs = { inherit inputs; username = "jrt"; inherit stateVersion; };
-        home-manager.useGlobalPkgs = true;
-        home-manager.useUserPackages = true;
-        home-manager.users.jrt = import ../home-manager;
-      }
-      mixins/cli
-      mixins/gui
-      mixins/backups
-      mixins/impermanence
-      mixins/laptop
-      mixins/secureboot
-      mixins/server
-      mixins/virt
-    ];
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  imports = [
+    # Include the results of the hardwae scan.
+    inputs.hyprland.nixosModules.default
+    inputs.impermanence.nixosModules.impermanence
+    inputs.lanzaboote.nixosModules.lanzaboote
+    inputs.chaotic.nixosModules.default # OUR DEFAULT MODULE
+    inputs.home-manager.nixosModules.home-manager
+    ./${hostname}
+    {
+      home-manager.extraSpecialArgs = {
+        inherit inputs;
+        username = "jrt";
+        inherit stateVersion;
+      };
+      home-manager.useGlobalPkgs = true;
+      home-manager.useUserPackages = true;
+      home-manager.users.jrt = import ../home-manager;
+    }
+    mixins/cli
+    mixins/gui
+    mixins/backups
+    mixins/impermanence
+    mixins/laptop
+    mixins/secureboot
+    mixins/server
+    mixins/virt
+  ];
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
 
   systemd.coredump.enable = false;
   boot.kernel.sysctl."kernel.core_pattern" = "|/bin/false";
   boot.kernel.sysctl."fs.suid_dumpable" = 0;
 
-	nix.gc = {
-		automatic = true;
-		dates = "daily";
-		options = "--delete-older-than 30d";
-	};
+  nix.gc = {
+    automatic = true;
+    dates = "daily";
+    options = "--delete-older-than 30d";
+  };
 
   boot.kernelPackages = pkgs.linuxPackages_latest;
   # boot.kernelPackages = pkgs.linuxPackages_testing;
@@ -61,15 +68,21 @@
   boot.initrd.verbose = false;
   boot.consoleLogLevel = 0;
   boot.kernelParams = [
-      "quiet"
-      "splash"
-      "loglevel=3"
-      "rd.systemd.show_status=false"
-      "rd.udev.log_level=3"
-      "udev.log_priority=3"
+    "quiet"
+    "splash"
+    "loglevel=3"
+    "rd.systemd.show_status=false"
+    "rd.udev.log_level=3"
+    "udev.log_priority=3"
   ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules = [
+    "xhci_pci"
+    "thunderbolt"
+    "nvme"
+    "usb_storage"
+    "sd_mod"
+  ];
   boot.initrd.kernelModules = [ ];
   networking.useDHCP = lib.mkDefault true;
   networking.networkmanager = {
@@ -85,31 +98,51 @@
 
   i18n.defaultLocale = "en_US.UTF-8";
   console = {
-     font = "Lat2-Terminus16";
-     keyMap = "us";
+    font = "Lat2-Terminus16";
+    keyMap = "us";
   };
 
   security.rtkit.enable = true;
 
+  # Create the 'hidraw' group if it doesn't exist.
+  users.groups.hidraw = { };
+
+  # Provide custom udev rules.
+  services.udev.extraRules = ''
+    KERNEL=="hidraw*", SUBSYSTEM=="hidraw", GROUP="hidraw", MODE="0660"
+  '';
+
   # Don't garbage collect flakes sources
-  system.extraDependencies = let
-      collectFlakeInputs = input: [ input ] ++ builtins.concatMap collectFlakeInputs (builtins.attrValues (input.inputs or {}));
-  in
-      builtins.concatMap collectFlakeInputs (builtins.attrValues inputs);
+  system.extraDependencies =
+    let
+      collectFlakeInputs =
+        input:
+        [ input ] ++ builtins.concatMap collectFlakeInputs (builtins.attrValues (input.inputs or { }));
+    in
+    builtins.concatMap collectFlakeInputs (builtins.attrValues inputs);
 
   users.users.jrt = {
-     isNormalUser = true;
-     extraGroups = [ "wheel" "tss" "input" "audio" "video" "ydotool" "libvirtd" ]; # Enable ‘sudo’ for the user.
-     packages = with pkgs; [
-       firefox
-       tree
-     ];
-     hashedPassword = "$6$JvSJ6iVd3.DRDc8e$lv.YEJaRy73l9RsiE6hmZm61Q0hH.cHo.QsFSGUsEjaS3n0EDnpzEqbaj6cNrYaw/9qnQLNo9TZ7RmipgBebw/";
-     description = "Jacob Root";
-     uid = 1001;
-     shell = pkgs.zsh;
+    isNormalUser = true;
+    extraGroups = [
+      "wheel"
+      "tss"
+      "input"
+      "audio"
+      "video"
+      "ydotool"
+      "libvirtd"
+      "hidraw"
+    ]; # Enable ‘sudo’ for the user.
+    packages = with pkgs; [
+      firefox
+      tree
+    ];
+    hashedPassword = "$6$JvSJ6iVd3.DRDc8e$lv.YEJaRy73l9RsiE6hmZm61Q0hH.cHo.QsFSGUsEjaS3n0EDnpzEqbaj6cNrYaw/9qnQLNo9TZ7RmipgBebw/";
+    description = "Jacob Root";
+    uid = 1001;
+    shell = pkgs.zsh;
     openssh.authorizedKeys.keys = [
-    	"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICPtgHM9vEd6NR70wKznoP/HE3aCrud/9rx/2Lu16Dh4 jrt@excelsior"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICPtgHM9vEd6NR70wKznoP/HE3aCrud/9rx/2Lu16Dh4 jrt@excelsior"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKrESH5ZwJ9UprxxlPHlwMTLZtNiFysHR+5CHcTA63+a jrt@constitution"
     ];
   };
