13b86511c8a73fd9eb78cae27a5303f9ebcc655d
Jacob Root <me@rooty.dev>
2025-02-01 21:43:16 -0800

    updated flake

--- flake.nix
+++ flake.nix
@@ -20,7 +20,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     lanzaboote = {
-      url = "github:nix-community/lanzaboote/v0.4.1";
+      url = "github:nix-community/lanzaboote/v0.4.2";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     zen-browser.url = "github:0xc000022070/zen-browser-flake";
