76d1e2af3396b6d39fd0d18852274d917213a807
Jacob Root <me@rooty.dev>
2026-01-21 23:04:21 -0800

    fix breakage caused by NixOS/nixpkgs#416165

--- nixos/modules/apps/sabnzbd.nix
+++ nixos/modules/apps/sabnzbd.nix
@@ -8,6 +8,7 @@
 let
   appName = "sabnzbd";
   cfg = config.modules.apps.${appName};
+  sabnzbdCfg = config.services.sabnzbd;
 in
 {
   options.modules.apps.${appName} = {
@@ -18,18 +19,87 @@ in
     openFirewall = lib.mkEnableOption "opening firewall ports for SABnzbd";
   };
 
-  config = lib.mkIf cfg.enable {
-    # Install SABnzbd
-    services.sabnzbd = {
-      enable = true;
-      inherit (cfg) openFirewall;
+  # Custom sabnzbd service options
+  options.services.sabnzbd = {
+    enable = lib.mkEnableOption "the sabnzbd server";
+
+    package = lib.mkPackageOption pkgs "sabnzbd" { };
+
+    configFile = lib.mkOption {
+      type = lib.types.path;
+      default = "/var/lib/sabnzbd/sabnzbd.ini";
+      description = "Path to config file.";
     };
 
-    # Persistence for SABnzbd
-    environment.persistence."/persist" = {
-      directories = [
-        "/var/lib/sabnzbd"
-      ];
+    user = lib.mkOption {
+      default = "sabnzbd";
+      type = lib.types.str;
+      description = "User to run the service as";
+    };
+
+    group = lib.mkOption {
+      type = lib.types.str;
+      default = "sabnzbd";
+      description = "Group to run the service as";
+    };
+
+    openFirewall = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = ''
+        Open ports in the firewall for the sabnzbd web interface
+      '';
     };
   };
+
+  config = lib.mkMerge [
+    # Custom sabnzbd service implementation
+    (lib.mkIf sabnzbdCfg.enable {
+      users.users = lib.mkIf (sabnzbdCfg.user == "sabnzbd") {
+        sabnzbd = {
+          uid = config.ids.uids.sabnzbd;
+          group = sabnzbdCfg.group;
+          description = "sabnzbd user";
+        };
+      };
+
+      users.groups = lib.mkIf (sabnzbdCfg.group == "sabnzbd") {
+        sabnzbd.gid = config.ids.gids.sabnzbd;
+      };
+
+      systemd.services.sabnzbd = {
+        description = "sabnzbd server";
+        wantedBy = [ "multi-user.target" ];
+        after = [ "network.target" ];
+        serviceConfig = {
+          Type = "forking";
+          GuessMainPID = "no";
+          User = sabnzbdCfg.user;
+          Group = sabnzbdCfg.group;
+          StateDirectory = "sabnzbd";
+          ExecStart = "${lib.getBin sabnzbdCfg.package}/bin/sabnzbd -d -f ${sabnzbdCfg.configFile}";
+        };
+      };
+
+      networking.firewall = lib.mkIf sabnzbdCfg.openFirewall {
+        allowedTCPPorts = [ 8080 ];
+      };
+    })
+
+    # Wrapper module configuration
+    (lib.mkIf cfg.enable {
+      # Install SABnzbd
+      services.sabnzbd = {
+        enable = true;
+        inherit (cfg) openFirewall;
+      };
+
+      # Persistence for SABnzbd
+      environment.persistence."/persist" = {
+        directories = [
+          "/var/lib/sabnzbd"
+        ];
+      };
+    })
+  ];
 }
